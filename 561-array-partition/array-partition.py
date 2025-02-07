eb28e24c22ad09c9a23fe2e4d0b5906835e3549b
Sneha Cherukuri <157361074+cherisn@users.noreply.github.com>
2025-02-06 21:56:14 -0500

    Time: 23 ms (87.36%) | Memory: 19.7 MB (30.92%) - LeetSync

--- 561-array-partition/array-partition.py
+++ 561-array-partition/array-partition.py
@@ -1,18 +1,3 @@
-#USED DEFAULT SORT IN PYTHON IF I NEED TO SORT IN DESC I NEED TO USE 
-#nums.sort(reverse=True)
-
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-
-        add = 0
-        i=0
-        while i < len(nums):  # Extra condition check every loop
-            add += nums[i]
-            i += 2  # Manually incrementing i 
-        return add
-"""
-        for i in range(0,len(nums),2):
-            add+=nums[i]
-        return add  
-"""
+        return sum(sorted(nums)[::2])
