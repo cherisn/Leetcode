c6343ff54dede5ac9f4990a836eea7db731253fa
Sneha Cherukuri <157361074+cherisn@users.noreply.github.com>
2025-02-06 21:55:34 -0500

    Time: 37 ms (17.90%) | Memory: 19.7 MB (59.25%) - LeetSync

--- 561-array-partition/array-partition.py
+++ 561-array-partition/array-partition.py
@@ -1,31 +1,18 @@
-"""
+#USED DEFAULT SORT IN PYTHON IF I NEED TO SORT IN DESC I NEED TO USE 
+#nums.sort(reverse=True)
+
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+
+        add = 0
+        i=0
+        while i < len(nums):  # Extra condition check every loop
+            add += nums[i]
+            i += 2  # Manually incrementing i 
+        return add
 """
-class Solution:
-    def arrayPairSum(self, nums: List[int]):
-        def partition(nums,low,high):
-            pivot=nums[high]
-            i=low-1
-            for j in range (low,high):
-                if nums[j]<=pivot:
-                    i+=1
-                    nums[i],nums[j]=nums[j],nums[i]
-            nums[i+1],nums[high]=nums[high],nums[i+1]
-            return i+1
-
-        def quicksort(nums,low=0,high=None):
-            if high is None:
-                high=len(nums)-1
-            if low<high:
-                pivot_index=partition(nums,low,high)
-                quicksort(nums,low,pivot_index-1)
-                quicksort(nums,pivot_index+1,high)
-        quicksort(nums)
-
-        n=0
-        for k in range(0,len(nums),2):
-            n+=nums[k]
-        return n
-
-        
+        for i in range(0,len(nums),2):
+            add+=nums[i]
+        return add  
+"""
