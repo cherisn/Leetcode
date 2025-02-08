c9c0261264e26f4a2f8dde45d75c4517bcfe9432
Sneha Cherukuri <157361074+cherisn@users.noreply.github.com>
2025-02-08 11:59:10 -0500

    Time: 734 ms (29.46%) | Memory: 26.3 MB (23.64%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,3 +1,28 @@
+#USED MERGE SORT
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return(sorted(nums))
+        def mergesort(nums):
+            if len(nums)<=1:
+                return nums
+            mid=len(nums)//2
+            lefthalf=nums[:mid]
+            righthalf=nums[mid:]
+            sortedleft=mergesort(lefthalf)
+            sortedright=mergesort(righthalf)
+
+            return merge(sortedleft,sortedright)
+
+        def merge(left,right):
+            result=[]
+            i=j=0
+            while i < len(left) and j < len(right):
+                if left[i]<right[j]:
+                    result.append(left[i])
+                    i+=1
+                else:
+                    result.append(right[j])
+                    j+=1
+            result.extend(left[i::])
+            result.extend(right[j::])
+            return result
+        return mergesort(nums)
